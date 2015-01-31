d3871ca6acd687081c65a2ff1949f2ef8956c0ef
paqo <0paqo0@gmail.com>
2015-01-31 04:07:45 +0100

    version definitiva del tamper monkey

--- src/main/java/com/reyma/gestion/util/monkey/reymasur-axa.js
+++ src/main/java/com/reyma/gestion/util/monkey/reymasur-axa.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Importar siniestros directamente desde pagina de AXA
 // @namespace  http://paqo.scripts.reyma/
-// @version    101
+// @version    110
 // @description  Incluye un boton debajo de los datos del encargo para importarlos en ReymaSur Gestion
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @match 	   http://localhost:8080/GDriveAPIJS/*
@@ -46,21 +46,12 @@ function obtenerParametros() {
     } else { // no se ha podido extraer ninguno
         params += "&perTlf1=" + $("#l_TelTomadorValue").text();
     }
-    var cpRaw = $("#l_DirecTomadorValue").text().match(/4\d{4}/);
-    var cp = "";
-    if (cpRaw && cpRaw.length == 1){
-        cp = cpRaw[0];    
-    	params += "&domCp=" + cp;       
-    }     
-    var provinciaRaw = $("#l_DirecTomadorValue").text().split(" ");
-    var provincia = provinciaRaw[provinciaRaw.length - 1];
-    if ( provincia ){        
-        params += "&provId=" + provincia.replace(',','').trim();    //PROVINCIA ASEGURADO: la provincia como tal (SEVILLA, etc..)
-    }    
-    var municipio = $("#l_DirecTomadorValue").text()
-    				.replace(provincia,'').trim().replace(',','').trim()
-    				.replace(cp,'').trim();    //MUNICIPIO ASEGURADO: la direccion del tomador sin CP ni provincia
-    params += "&munId=" + municipio;
+    
+    var prov_mun_cp_aseg = obtenerProvMunicipioCP( $("#l_DirecTomadorValue").text().trim(), "asegurado" );
+    params += "&provId=" + prov_mun_cp_aseg[0];
+    params += "&munId=" + prov_mun_cp_aseg[1];
+    params += "&domCp=" + prov_mun_cp_aseg[2];
+    
     if ($("#l_TipoLugarValue").text().toUpperCase() === "DOMICILIO ASEGURADO") {
         params += "&mismolugar=true";
         // si el domicilio del asegurado es el del perjudicado, 
@@ -73,24 +64,66 @@ function obtenerParametros() {
         params += "&perjNombre="  +$("#l_NombreLugarValue").text();
         params += "&perjDireccion=" + $("#HeaderAssignment1_lblDir_Riesgo").text().trim(); 
         params += "&perjTlf1=" + $("#l_TelefLugarValue").text().replace('(FIJO)','').trim();
-        params += "&perjTlf2=" + $("#l_TelefLugarValue2").text().replace('(MOVIL)','').trim();        
-        var provinciaPerjRaw = $("#l_DirecTomadorValue").text().split(" ");
-        var provinciaPerj = provinciaPerjRaw[provinciaPerjRaw.length - 1];
-        if ( provinciaPerj ){        
-            params += "&perjProvId=" + provinciaPerj.replace(',','').trim();
-        }    	
-        var cpPerj = $("#l_DireccionLugarVaue").text().match(/4\d{4}/);
-        if (cpPerj && cpPerj.length == 1){            
-            params += "&perjCp=" + cpPerj[0];       
-        }
-        var municipioPerj = $("#l_DireccionLugarVaue").text()
-    						.replace(provinciaPerj,'').trim().replace(',','').trim()
-    						.replace(cpPerj,'').trim();
-        params += "&perjMunId=" + municipio;
+        params += "&perjTlf2=" + $("#l_TelefLugarValue2").text().replace('(MOVIL)','').trim();
+        //var prov_mun_cp_perj = obtenerProvMunicipioCP( $("#HeaderAssignment1_lblDir_Riesgo").text().trim() );
+        var prov_mun_cp_perj = obtenerProvMunicipioCP( $("#l_DireccionLugarVaue").text().trim(), "perjudicado" );        
+        params += "&perjProvId=" + prov_mun_cp_perj[0];
+        params += "&perjMunId=" + prov_mun_cp_perj[1];
+        params += "&perjCp=" + prov_mun_cp_perj[2];
     }
     console.log("-> params: " + params);
     
     return params;
+}
+
+function obtenerProvMunicipioCP(direccion, tipo) {
+	// sincrono...
+	$.ajaxSetup({
+	    async: false
+	});
+	
+	var resultado = [];
+	var googleApi = "https://maps.googleapis.com/maps/api/geocode/json";
+	$.getJSON( googleApi, {
+		address : direccion,
+		region: "es",
+		sensor: "false"
+	})
+	.error(function( data ) {
+		console.log("error recibiendo respuesta de google" );
+	}).success(function( data ) {
+		if ( data && data.status == 'OK' ){
+			var direccionFormateada = data.results[0].formatted_address;
+			console.log("=> direccionFormateada de " + tipo + ": " + direccionFormateada);			
+			var precision = data.results[0].geometry.location_type;
+            console.log("precision: " + precision);
+            var result = direccionFormateada.split(",");
+			//console.log("=> " + result[result.length-3]);
+			var provincia = result[result.length-2];
+			var mun_CP = result[result.length-3];
+			var cp = mun_CP.match(/4\d{4}/);
+			resultado.push(provincia.trim());
+			resultado.push(mun_CP.replace(cp,"").trim());
+			resultado.push(cp);
+            if (precision == "APPROXIMATE" || precision == "GEOMETRIC_CENTER"){ 
+            	// alta probabilidad de error en 
+				// CP y municipio, avisar
+            	if (cp == null){
+            		resultado[2]="";
+            		alert("no se ha podido obtener el cp del domicilio del " + tipo + "\n(comprobar manualmente CP y municipio del " + tipo + ")");
+            	}else {
+            		alert("comprobar municipio y cp en domicilio de " + tipo + ":\n" + mun_CP + "?");
+            	}
+            	
+			}            
+		} 
+	});
+	// asincrono de nuevo
+	$.ajaxSetup({
+	    async: true
+	});
+	console.log("=> resultado: " + resultado);
+	return resultado;
 }
 
 console.log("################################################");
